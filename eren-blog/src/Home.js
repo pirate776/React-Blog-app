e521be38483bfee4006f6f3cdfb45bd8ef28d8f9
pirate776 <kingzaljaguar@gmail.com>
2024-03-01 13:56:48 +0300

    bloglist-component

--- eren-blog/src/Home.js
+++ eren-blog/src/Home.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import BlogList from './BlogList';
 function Home() {
 
   const [blogs, setBlogs] = useState([
@@ -9,16 +10,7 @@ function Home() {
   
     return (
       <div className="home">
-        {
-          blogs.map((blog)=>(
-            <div className="blog-preview" key={blog.id}>
-              <h2>{blog.title}</h2>
-              <p>written by {blog.author}</p>
-            </div>
-          )
-
-          )
-        } 
+        <BlogList blogs = {blogs} />
       </div>
     );
   }
